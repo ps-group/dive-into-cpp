50efe8644f3e28311ba10dbd1a7dcec37e6119ce
student <student@school.isphera.ru>
2018-03-05 13:57:14 +0300

    Added Multiply vectors on scal

--- vector3/Vector3f_tests.cpp
+++ vector3/Vector3f_tests.cpp
@@ -23,15 +23,37 @@ TEST_CASE("Can be constructed", "[Vector3f]")
     REQUIRE(v2.z == 42);
 
     // Конструирование списком инициализации (C++11) - более универсальный приём.
-    Vector3f v3 = { 5, -11, -5.5f };
+    Vector3f v3 = {5, -11, -5.5f};
     REQUIRE(v3.x == 5);
     REQUIRE(v3.y == -11);
     REQUIRE(v3.z == -5.5f);
 
     // Универсальное конструирование (C++11) - ещё более универсальное
-    Vector3f v4 = { 18, -110, 0.1f };
+    Vector3f v4 = {18, -110, 0.1f};
     REQUIRE(v4.x == 18);
     REQUIRE(v4.y == -110);
     REQUIRE(v4.z == 0.1f);
 }
 
+TEST_CASE("Multiply vectors on scal", "[Vector3f]")
+{
+    Vector3f v1 = Vector3f{2, 3, 4} * (-4);
+    REQUIRE(v1.x == -8);
+    REQUIRE(v1.y == -12);
+    REQUIRE(v1.z == -16);
+
+    Vector3f v2 = Vector3f{10, -6, 2} * 1;
+    REQUIRE(v2.x == 10);
+    REQUIRE(v2.y == -6);
+    REQUIRE(v2.z == 2);
+
+    Vector3f v3 = Vector3f{1, -1, 2} * 2;
+    REQUIRE(v3.x == 2);
+    REQUIRE(v3.y == -2);
+    REQUIRE(v3.z == 4);
+
+    Vector3f v4 = Vector3f{0, -3, 8} * 2;
+    REQUIRE(v4.x == 0);
+    REQUIRE(v4.y == -6);
+    REQUIRE(v4.z == 16);
+}
